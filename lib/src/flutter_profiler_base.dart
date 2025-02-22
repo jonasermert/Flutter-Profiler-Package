da2701e51c23f9f26f2799773a8eb6bcfe8ac163
Jonas <ermert.jonas@yahoo.de>
2025-02-22 13:06:19 +0100

    update pubspec.yaml and implement flutter_profile_base.dart

--- lib/src/flutter_profiler_base.dart
+++ lib/src/flutter_profiler_base.dart
@@ -1,6 +1,45 @@
-// TODO: Put public facing types in this file.
+library flutter_profiler;
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'dart:developer';
+
+class FlutterProfiler {
+  static final Map<String, Stopwatch> _stopwatches = {};
+  static final List<Map<String, dynamic>> _timeline = [];
+
+  static void start(String key) {
+    if (_stopwatches.containsKey(key)) {
+      log('Profiler Warning: Task "$key" is already running.');
+      return;
+    }
+    _stopwatches[key] = Stopwatch()..start();
+  }
+
+  static void stop(String key) {
+    if (!_stopwatches.containsKey(key)) {
+      log('Profiler Warning: Task "$key" was not started.');
+      return;
+    }
+
+    final stopwatch = _stopwatches.remove(key)!;
+    stopwatch.stop();
+    final duration = stopwatch.elapsedMilliseconds;
+
+    _timeline.add({
+      'key': key,
+      'duration': duration,
+      'timestamp': DateTime.now().toIso8601String(),
+    });
+
+    log('Profiler: Task "$key" completed in ${duration}ms');
+  }
+
+  static List<Map<String, dynamic>> getTimeline() {
+    return List.unmodifiable(_timeline);
+  }
+
+  static void clear() {
+    _stopwatches.clear();
+    _timeline.clear();
+    log('Profiler: Timeline cleared.');
+  }
 }
